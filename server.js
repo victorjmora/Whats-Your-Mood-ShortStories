d323e2d293e108fb2bd3cc1652f558faf583c498
James McConnaughey <jbmcconnaughey@gmail.com>
2023-05-29 14:16:28 -0400

    additional debugging in server.js, seed.js, storyroutes.js

--- server.js
+++ server.js
@@ -11,6 +11,9 @@ const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+// Set up Handlebars.js engine with custom helpers
+const hbs = exphbs.create({ helpers });
+
 const sess = {
   secret: 'Super secret secret',
   cookie: {
