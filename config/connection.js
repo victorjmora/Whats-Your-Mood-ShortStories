e22cd8eaa22733c5f529c9df78c5c688f7dfb34a
victormora1 <victorjmora1@gmail.com>
2023-06-06 15:54:50 -0400

    added media query so the webpage can also fit into tablet and smartphone screens

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,7 @@
 const Sequelize = require('sequelize');
 require('dotenv').config();
 
+
 let sequelize;
 
 if (process.env.JAWSDB_URL) {
@@ -13,7 +14,7 @@ if (process.env.JAWSDB_URL) {
     {
       host: 'localhost',
       dialect: 'mysql',
-      port: 3306
+      port: 3001
     }
   );
 }
